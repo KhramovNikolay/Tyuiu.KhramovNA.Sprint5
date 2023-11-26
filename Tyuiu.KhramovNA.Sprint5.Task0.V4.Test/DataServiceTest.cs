dc2e10ba3832095c526588eef639fb63f149c498
Николай Храмов <Tyuiu.KhramovN@outlook.com>
2023-11-26 21:09:38 +0500

    Add project files.

---
+++ Tyuiu.KhramovNA.Sprint5.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.KhramovNA.Sprint5.Task0.V4.Lib;
+namespace Tyuiu.KhramovNA.Sprint5.Task0.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\nikol\source\repos\Tyuiu.KhramovNA.Sprint5\Tyuiu.KhramovNA.Sprint5.Task0.V4\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KhramovNA.Sprint5.Task0.V4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KhramovNA.Sprint5.Task0.V4.Lib;
+namespace Tyuiu.KhramovNA.Sprint5.Task0.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Храмов Н. А. | АСОиУБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Храмов Николай Андреевич | АСОиУБ-23-3                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение выражения при x = 2.     *");
+            Console.WriteLine("* Результат сохранить в текстовый файл OutPutFileTask0.txt и вывести      *");
+            Console.WriteLine("* на консоль. Округлить до трёх знаков после запятой.                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double y = 2 * Math.Pow(x, 3) + 0.5 * Math.Pow(x, 2) - 3.5 * x + 2;
+            y = Math.Round(y, 3);
+            Console.WriteLine("Ответ: " + y);
+
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Фaйл: " + res);
+            Console.WriteLine("Coздан!");
+            Console.ReadKey();
+        }
+    }
+}
