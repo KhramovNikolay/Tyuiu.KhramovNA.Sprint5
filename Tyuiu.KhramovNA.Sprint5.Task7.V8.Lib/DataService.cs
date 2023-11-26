b54316ff96ffa8397b64c1b802102befc8dd280d
Николай Храмов <Tyuiu.KhramovN@outlook.com>
2023-11-26 21:25:32 +0500

    Task7

---
+++ Tyuiu.KhramovNA.Sprint5.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KhramovNA.Sprint5.Task7.V8.Lib
+{
+    public class DataService : ISprint5Task7V8
+    {
+        public string LoadDataAndSave(string path)
+        {
+
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\InPutFileTask7.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+
+                File.Delete(pathSaveFile);
+            }
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+
+
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] >= 192 && line[i] <= 255)
+                        {
+                            strLine = strLine + line[i];
+                        }
+
+
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.KhramovNA.Sprint5.Task7.V8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KhramovNA.Sprint5.Task7.V8.Lib;
+namespace Tyuiu.KhramovNA.Sprint5.Task7.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Храмов Н. А. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание: 7                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Храмов Николай Андреевич | АСОиУБ-23-3                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Заменить все заглавные русские буквы на строчные.                       *");
+            Console.WriteLine("* Полученный результат сохранить в файл OutPutDataFileTask7V8.txt         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask7V8.txt";
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V8.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Находится в файле: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+
+        }
+    }
+}
