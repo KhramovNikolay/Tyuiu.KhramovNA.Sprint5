67716c56b7670afff83359e6d0138559ed123c10
Николай Храмов <Tyuiu.KhramovN@outlook.com>
2023-11-26 21:16:41 +0500

    Task3

---
+++ Tyuiu.KhramovNA.Sprint5.Task3.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KhramovNA.Sprint5.Task3.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KhramovNA.Sprint5.Task3.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18c262ba-cce7-4e76-8222-c284638f1d63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
